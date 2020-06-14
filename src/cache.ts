100306dfc101ab7909d6374890290a8238799df9
Kareem Abdel Hadi <kareem.abdelhadi@daybreakhotels.com>
2020-06-14 21:42:59 +0200

    summrize introduction

--- src/cache.ts
+++ src/cache.ts
@@ -1,104 +1,72 @@
-
-/**
- * 
+/*
+ *
  * init cache as map obj
- * 
+ *
  */
 
-
-// use immtablejs 
-
+// use immtablejs
 const cache = new Map();
 
-/**
- * 
- * cash id for current state
- * 
- */
+// cash id for current state
 export const injectState = <T>(id: string, initialState: T) => {
   id = (id || "").toString();
 
-
-  /**
-   * 
-   * if no id we stop appending 
-   * 
-   */
-
-  if(!id){
+  // if no id we stop appending
+  if (!id) {
     return;
-  };
+  }
 
+  // if cash has current id
+  if (cache.has(id)) {
+    throw new Error(`${id} id exsists`);
+  }
 
-  /**
-   * 
-   * if cash has current id
-   * 
-   */
-
-  if(cache.has(id)){
-    throw new Error(`${id} id exsists`)
-  };
-  
-  /**
-   * 
-   * if cash has't current id we append new entry it with initial value
-   * 
-   */
-  
-   if(!cache.has(id)){
-    
+  // if cash has't current id we append new entry it with initial value
+  if (!cache.has(id)) {
     cache.set(id, initialState);
 
-    return
-  };
+    return;
+  }
 };
 
+// update cach state with given id
+export const update = <T>(id: string, newValues: T) => {
+  id = (id || "").toString();
 
-/**
- * 
- * update cach state with given id
- * 
- */
+  if (!id) {
+    return null;
+  }
 
- export const update = <T>(id: string, newValues: T) => {
-    id = (id || "").toString();
+  let currentIdCachedValues = cache.get(id);
 
-    if(!id){
-      return null;
-    }  
-  
-    let currentIdCachedValues = cache.get(id);
+  // 1- user will send partial state to update
+  // 2- user will send whole state to update
+  if (Array.isArray(currentIdCachedValues)) {
+    currentIdCachedValues = [...currentIdCachedValues, newValues];
 
-
-    // 1- user will send patirail state to update
-    // 1- user will send whole state to update
-
-    if(Array.isArray(currentIdCachedValues)){
-      currentIdCachedValues = [...currentIdCachedValues, newValues];
-
-      cache.set(id, currentIdCachedValues);
-      return currentIdCachedValues
-    }
-
-    if(!Array.isArray(currentIdCachedValues) && typeof currentIdCachedValues === "object"){
-      currentIdCachedValues = { ...currentIdCachedValues, ...newValues }
-
-      cache.set(id, currentIdCachedValues);
-      return currentIdCachedValues
-    }
-
-    currentIdCachedValues = newValues;
-    
     cache.set(id, currentIdCachedValues);
+    return currentIdCachedValues;
+  }
 
-    return currentIdCachedValues
- };
+  if (
+    !Array.isArray(currentIdCachedValues) &&
+    typeof currentIdCachedValues === "object"
+  ) {
+    currentIdCachedValues = { ...currentIdCachedValues, ...newValues };
 
+    cache.set(id, currentIdCachedValues);
+    return currentIdCachedValues;
+  }
 
- export default {
+  currentIdCachedValues = newValues;
+
+  cache.set(id, currentIdCachedValues);
+
+  return currentIdCachedValues;
+};
+
+export default {
   data: cache,
   update,
-  injectState
- }
- 
+  injectState,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,82 @@
-import * as R from 'react';
-import cache from './cache';
-import { SubscriberFunc, ConsumerResult, ResultProps, ConsumerProps } from './interface';
+//TODO:  a research needed to understand the side effect of ignoring `React` namespace.
+import * as R from "react";
+import cache from "./cache";
+import {
+  SubscriberFunc,
+  ConsumerResult,
+  ResultProps,
+  ConsumerProps,
+} from "./interface";
 
-
-
-
-/**
- * 
- * initialize provider
- */
-
+//  initialize provider.
 export const stateConsumer = <T>({
   id,
-  initialState
+  initialState,
 }: ConsumerProps<T>): ResultProps<T> => {
-  
   let listeners: SubscriberFunc<T>[] = [];
 
-    // set function
-    // eslint-disable-next-line
-    const callListeners = (newValue: T, callback: (newValue: T) => void) => {
-      
-      // update the cash
-      cache.update(id, newValue);
+  // Set function.
+  // eslint-disable-next-line
+  const callListeners = (newValue: T, callback: (newValue: T) => void) => {
+    // update the cash
+    cache.update(id, newValue);
 
-      // notify caller subscriber direct
-      if(callback){
-        callback(newValue);
+    // notify caller subscriber direct.
+    if (callback) {
+      callback(newValue);
+    }
+
+    // Let subscribers know value did change async.
+    setTimeout(() => {
+      // call subscribers which are not the caller.
+      listeners.forEach(
+        (fn: SubscriberFunc<T>) => fn !== callback && fn(newValue)
+      );
+    });
+  };
+
+  //TODO: Check `useConsumerState` re-execution.
+  const useConsumerState = (nextId?: string): ConsumerResult<T> => {
+    const [state, setState] = R.useState<T>(initialState as T);
+
+    // Add new entry state by `id` and add it to cache by the given `id`,
+    // if it does not exsists.
+    R.useEffect(() => {
+      if (!cache.data.has(id)) {
+        cache.injectState(id, initialState);
       }
 
-      // let subscribers know value did change async
-      setTimeout(() => {
-        // call subscribers which are not the caller
-        listeners.forEach((fn: SubscriberFunc<T>) => fn !== callback && fn(newValue));
-      });
-    };
+      if (nextId !== id) {
+        setState(cache.data.get(id));
 
-
-    const useConsumerState = (): ConsumerResult<T> => {
-      const [state, setState] = R.useState<T>(initialState as T);
-
-      /**
-       * add new entry state by id if not exsist 
-       * init state with cahced data by given id
-       * 
-       */
-
-      R.useEffect(() => {
-        if(!cache.data.has(id)){
-          cache.injectState(id, initialState)
-        }
-    
-        setState(cache.data.get(id))
-    
         listeners.push(setState);
-    
-        return () => {
-          // eslint-disable-next-line
-          listeners = listeners.filter((f) => f !== setState);
-        };
-    
-      }, [setState, id, initialState])
+      }
 
+      return () => {
+        // eslint-disable-next-line
+        listeners = listeners.filter((f) => f !== setState);
+      };
+    }, [setState, id, initialState, nextId]);
 
-      /**
-       * update state and call Listeners
-       */
-      const updater = R.useCallback((preStateOrNextValue: T | SubscriberFunc<T>) => {
-        // support previous as argument to new value
-      const newValue: T  = preStateOrNextValue instanceof Function ? preStateOrNextValue(state) : preStateOrNextValue;
-
+    // update state and call Listeners.
+    const updater = R.useCallback(
+      (preStateOrNextValue: T | SubscriberFunc<T>) => {
+        // support previous as argument to new value.
+        const newValue: T =
+          preStateOrNextValue instanceof Function
+            ? preStateOrNextValue(state)
+            : preStateOrNextValue;
         callListeners(newValue, setState);
+      },
+      [setState, state]
+    );
 
-      }, [setState, state])
-
-      return [state, updater, cache.data];
-    };
-
+    return [state, updater, cache.data];
+  };
 
   return {
     useConsumerState,
-    useValue: () => useConsumerState()[0],
+    useValue: () => useConsumerState(id)[0],
     getCach: () => cache.data,
-  }
+  };
 };
-
-export default stateConsumer;
-
-
-
